7e63f5f59e5f3c64bea4e53cb3170133b288e485
Tiago <tiagoalex0202@gmail.com>
2023-07-17 12:04:27 +0100

    docs: update

--- src/controllers/hotel-controller.ts
+++ src/controllers/hotel-controller.ts
@@ -4,16 +4,17 @@ import httpStatus from 'http-status';
 import { AuthenticatedRequest } from '@/middlewares';
 
 export async function getHotels (req : AuthenticatedRequest, res : Response) {
-    const result = await HotelServices.getHotels()
+    const {userId} = req;
+    const result = await HotelServices.getHotels(userId)
     return res.status(httpStatus.OK).send(result);
 }
 
 export async function getHotelInfo (req: AuthenticatedRequest, res: Response) {
-
+    const {userId} = req;
     const { hotelId } = req.params;
     const Id = Number(hotelId)
     
-    const result = await HotelServices.getHotelInfo(Id)
+    const result = await HotelServices.getHotelInfo(Id, userId)
 
     return res.status(httpStatus.OK).send(result)
 }
